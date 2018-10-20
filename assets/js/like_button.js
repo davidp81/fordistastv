8f76e7c45107fe87e329743c625dc51d0a55fab2
davidp81 <david@product81.com>
2018-10-20 19:39:03 -0400

    added react.js

--- assets/js/like_button.js
+++ assets/js/like_button.js
@@ -10,13 +10,13 @@ class LikeButton extends React.Component {
 
   render() {
     if (this.state.liked) {
-      return 'You liked this.';
+      return 'Have a great day!.';
     }
 
     return e(
       'button',
       { onClick: () => this.setState({ liked: true }) },
-      'Like'
+      'Do not Click this'
     );
   }
 }
